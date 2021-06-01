4cc51757d04470ee38e471260c378dfdbef57a34
Sergey <52304443+c29r3@users.noreply.github.com>
2021-06-01 17:37:34 +0300

    Update node-restarter.sh

--- node-restarter.sh
+++ node-restarter.sh
@@ -19,7 +19,7 @@ RED="\e[91m"
 NORMAL="\e[39m"
 
 
-EXPLORER_HEIGTH=$(curl -s https://api.minaexplorer.com/summary | jq -r .blockchainLength)
+#EXPLORER_HEIGTH=$(curl -s https://api.minaexplorer.com/summary | jq -r .blockchainLength)
 STATUS_DATA=$(docker exec mina mina client status --json | grep -v "Using password from")
 MAX_UNVALIDATED_BLOCK=$(jq .highest_unvalidated_block_length_received <<< $STATUS_DATA)
 LOCAL_HEIGHT=$(jq .blockchain_length <<< $STATUS_DATA)
@@ -29,13 +29,13 @@ SYNC_STATUS=$(jq .sync_status <<< $STATUS_DATA)
 
 echo -e "\n-------------------------"
 echo -e $(date)
-echo -e "Local/Explorer: ${LOCAL_HEIGHT}\\${EXPLORER_HEIGTH}"       
+echo -e "Local/Explorer: ${LOCAL_HEIGHT}\\${MAX_UNVALIDATED_BLOCK}"       
 echo -e "Uptime: ${UPTIME} | Status: ${SYNC_STATUS}"   
 
 if [[ $(bc -l <<< "${MAX_UNVALIDATED_BLOCK} - ${LOCAL_HEIGHT}") -gt ${SYNC_WINDOW} ]] && [[ ${UPTIME} -gt ${MIN_UPTIME} ]]
   then
     echo -e ${RED}"ALARM! ${CONTAINER_NAME} node on ${HOSTNAME} is out of sync"${NORMAL}
-    MSG=$(echo -e "${CONTAINER_NAME} node on ${HOSTNAME} is out of sync\nLocal/Explorer: ${LOCAL_HEIGHT}/${EXPLORER_HEIGTH}\nUptime: ${UPTIME} | Status: ${SYNC_STATUS}")
+    MSG=$(echo -e "${CONTAINER_NAME} node on ${HOSTNAME} is out of sync\nLocal/Explorer: ${LOCAL_HEIGHT}/${MAX_UNVALIDATED_BLOCK}\nUptime: ${UPTIME} | Status: ${SYNC_STATUS}")
     
     if [[ ${TG_TOKEN} != "" ]]
       then
